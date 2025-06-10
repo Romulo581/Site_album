a9125bfdf037b188c0c0af1124ed9baa66ae0da7
Romulo581 <romulo.da508@gmail.com>
2025-06-09 22:01:55 -0300

    teste17

--- javaScript/script.js
+++ javaScript/script.js
@@ -150,9 +150,9 @@ function ajustarParaViewportReal() {
   const botao = document.querySelector('.play_musica');
 
   if (altura < 360) {
-    carrossel.style.marginTop = '40px';
+    carrossel.style.marginTop = '45px';
     carrossel.style.height = '45vh';
-    botao.style.marginTop = '20px';
+    botao.style.marginTop = '25px';
   }
 }
 
